1b235cd410bc45b74fec27f7ad54bbb85cfbb4ce
josepCortesManyanich <gaara96@gmail.com>
2024-04-11 18:38:21 +0200

    first commit

---
+++ src/Componentes/CrearTarea.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import { useState } from 'react'
+
+
+function CrearTarea() {
+  const [name, setName]= useState('')
+  const [tarea, setTarea]= useState('')
+  const [description, setDescription]= useState('')
+  const [estado, setEstado]= useState('')
+
+
+  
+    return (
+    <div>
+       <label>Nombre</label>
+                <input type="text" placeholder="Name" name="name" value={name} onChange={(e) => {setName(e.target.value)}} />
+        <label>Quien la tiene que hacer</label>
+        <select value={tarea} name="tarea" onChange={(e) => {setTarea(e.target.value)}}>
+                    <option value="Raquel">Raquel </option>
+                    <option value="Josep">Josep </option>
+        </select>
+                
+        <label>Description</label>
+                <input type="text" placeholder="Description" name="description" value={description} onChange={(e) => {setDescription(e.target.value)}}/>
+        <label>Estado</label>
+            <select value={estado} name="done" onChange={(e) => {setDone(e.target.value)}}>
+                    <option value="Realizada">👌 </option>
+                    <option value="Por hacer">❌ </option>
+            </select>
+                
+    </div>
+  )
+}
+
+export default CrearTarea
